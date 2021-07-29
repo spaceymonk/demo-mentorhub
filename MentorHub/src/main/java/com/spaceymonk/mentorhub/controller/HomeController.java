19a06b64b215bc7bd61a08711e8711884bccd5bd
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-29 19:02:46 +0300

    OAuth2 done.

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/HomeController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/HomeController.java
@@ -1,8 +1,18 @@
 package com.spaceymonk.mentorhub.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/dashboard")
+    public String dashboardPage(Model model, @AuthenticationPrincipal OAuth2User user) {
+        model.addAllAttributes(user.getAttributes());
+        return "dashboard";
+    }
 
 }
