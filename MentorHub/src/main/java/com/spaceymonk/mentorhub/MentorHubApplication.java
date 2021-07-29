83bf86d57df9f7e9c0a941b9c5942e4ebb55efd3
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-29 11:28:11 +0300

    Project initialized.

---
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/MentorHubApplication.java
@@ -0,0 +1,13 @@
+package com.spaceymonk.mentorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorHubApplication.class, args);
+    }
+
+}
