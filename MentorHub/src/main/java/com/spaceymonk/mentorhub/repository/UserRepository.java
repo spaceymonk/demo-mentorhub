dff803a776a1c02b31ea2049c63a7ac678afb8a9
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 16:09:17 +0300

    created User domain

---
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.spaceymonk.mentorhub.repository;
+
+import com.spaceymonk.mentorhub.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    public User findByName(String name);
+}
