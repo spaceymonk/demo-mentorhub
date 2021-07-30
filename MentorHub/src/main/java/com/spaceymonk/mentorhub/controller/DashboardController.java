07e593c46b703056e37705a3193ee234018bd8c5
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 12:12:36 +0300

    dummy parameter test

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class DashboardController {
     @GetMapping("/dashboard")
-    public String dashboardPage(Model model) {
+    public String dashboardPage(Model model, Principal principal) {
+        model.addAttribute("name", principal.getName());
         return "dashboard";
     }
 }
