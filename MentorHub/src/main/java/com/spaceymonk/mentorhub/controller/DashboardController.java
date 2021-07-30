119aa43f8865c3430d61e06fc12648e98ccac363
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 15:21:00 +0300

    moved to features folder

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
@@ -1,7 +1,5 @@
 package com.spaceymonk.mentorhub.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +11,6 @@ public class DashboardController {
     @GetMapping("/dashboard")
     public String dashboardPage(Model model, Principal principal) {
         model.addAttribute("name", principal.getName());
-        return "dashboard";
+        return "features/dashboard";
     }
 }
