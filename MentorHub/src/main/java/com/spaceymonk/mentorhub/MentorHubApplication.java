16fed34da2ed5480acc6b6603aa1ffc9627ed7b3
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 16:10:03 +0300

    Bootstrap code added

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/MentorHubApplication.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/MentorHubApplication.java
@@ -1,13 +1,27 @@
 package com.spaceymonk.mentorhub;
 
+import com.spaceymonk.mentorhub.repository.UserRepository;
+import com.spaceymonk.mentorhub.domain.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MentorHubApplication {
+public class MentorHubApplication implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public MentorHubApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MentorHubApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.save(new User("Ralph", "Admin"));
+        System.out.println(userRepository.findAll());
+    }
 }
