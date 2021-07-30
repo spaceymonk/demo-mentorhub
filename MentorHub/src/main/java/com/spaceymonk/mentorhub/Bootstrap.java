df1d51c0f6d9bd25ccaa3e104721412344f1c486
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 17:30:34 +0300

    Bootstrap code seperated

---
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/Bootstrap.java
@@ -0,0 +1,25 @@
+package com.spaceymonk.mentorhub;
+
+import com.spaceymonk.mentorhub.domain.User;
+import com.spaceymonk.mentorhub.repository.*;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class Bootstrap implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final MentorshipRepository mentorshipRepository;
+    private final PhaseRepository phaseRepository;
+    private final PhaseReviewRepository phaseReviewRepository;
+    private final RequestRepository requestRepository;
+    private final SubjectRepository subjectRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started in bootstrap...");
+
+    }
+}
--- MentorHub/src/main/java/com/spaceymonk/mentorhub/MentorHubApplication.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/MentorHubApplication.java
@@ -1,27 +1,11 @@
 package com.spaceymonk.mentorhub;
 
-import com.spaceymonk.mentorhub.repository.UserRepository;
-import com.spaceymonk.mentorhub.domain.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MentorHubApplication implements CommandLineRunner {
-
-    private final UserRepository userRepository;
-
-    public MentorHubApplication(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
+public class MentorHubApplication {
     public static void main(String[] args) {
         SpringApplication.run(MentorHubApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        userRepository.save(new User("Ralph", "Admin"));
-        System.out.println(userRepository.findAll());
     }
 }
