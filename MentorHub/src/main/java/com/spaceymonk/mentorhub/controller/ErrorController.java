c52fc953d8cb522674367997c086c52147e2055c
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 09:20:40 +0300

    moved to template html files, created endpoints

---
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/ErrorController.java
@@ -0,0 +1,17 @@
+package com.spaceymonk.mentorhub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController {
+    @GetMapping("/error")
+    public String errorPage(Model model, HttpServletRequest request) {
+        String errMsg = (String) request.getSession().getAttribute("error.message");
+        model.addAttribute("msg", errMsg);
+        return "error";
+    }
+}
--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/HomeController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.spaceymonk.mentorhub.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/dashboard")
-    public String dashboardPage(Model model, @AuthenticationPrincipal OAuth2User user) {
-        model.addAllAttributes(user.getAttributes());
-        return "dashboard";
+    @GetMapping("/")
+    public String loginPage() {
+        return "index";
     }
 
 }
