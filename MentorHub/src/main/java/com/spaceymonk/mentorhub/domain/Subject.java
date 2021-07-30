186220ea67d2eb2871f024251a8ddc46aa3cfd8a
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-30 19:28:10 +0300

    id field added

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/domain/Subject.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/domain/Subject.java
@@ -1,6 +1,7 @@
 package com.spaceymonk.mentorhub.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,6 +9,8 @@ import java.util.Set;
 @Data
 public class Subject {
 
+    @Id
+    private String id;
     private String majorSubject;
     private Set<String> subjects = new HashSet<>();
 
