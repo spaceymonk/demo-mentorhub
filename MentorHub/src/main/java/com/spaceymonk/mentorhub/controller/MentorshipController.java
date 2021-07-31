979d707fdc4cd359b268ba0bd6e26e86bb19ec81
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-31 13:50:25 +0300

    application form back-end connection

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/MentorshipController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/MentorshipController.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,9 +19,20 @@ public class MentorshipController {
 
     @GetMapping("/apply")
     public String applyPage(Model model) {
-        List<Subject> categories =  subjectRepository.findAll();
+        List<Subject> categories = subjectRepository.findAll();
         model.addAttribute("categories", categories);
         return "features/mentor-application";
+    }
+
+    @PostMapping("/apply")
+    public String registerApplication(@RequestParam("selectedCategory") String selectedCategory,
+                                      @RequestParam("selectedSubject") List<String> selectedSubjects,
+                                      @RequestParam("explainMsg") String explainMsg) {
+        System.out.println(selectedCategory);
+        for (String s : selectedSubjects)
+            System.out.println(s);
+        System.out.println(explainMsg);
+        return "redirect:/";
     }
 
 }
