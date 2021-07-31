162d9a9e5c8a158d9dc5f6f32a43f6ae1e3032cb
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-07-31 22:38:40 +0300

    error handling pages

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/ErrorController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/ErrorController.java
@@ -1,5 +1,6 @@
 package com.spaceymonk.mentorhub.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +15,10 @@ public class ErrorController {
         request.getSession().removeAttribute("error.message");
         model.addAttribute("msg", errMsg);
         return "error";
+    }
+
+    @GetMapping("/403")
+    public String accessDeniedPage(Model model, Authentication authentication) {
+        return "errors/403";
     }
 }
