75af6c01ac8a29e92a62ca8030c915c0839a5bf0
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-01 19:54:08 +0300

    added findByMentor method

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/MentorshipRequestRepository.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/MentorshipRequestRepository.java
@@ -1,10 +1,12 @@
 package com.spaceymonk.mentorhub.repository;
 
 import com.spaceymonk.mentorhub.domain.MentorshipRequest;
+import com.spaceymonk.mentorhub.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
 public interface MentorshipRequestRepository extends MongoRepository<MentorshipRequest, String> {
     List<MentorshipRequest> findByStatus(String status);
+    List<MentorshipRequest> findByMentor(User mentor);
 }
