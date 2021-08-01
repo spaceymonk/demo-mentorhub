e74041f8d3c388b73bf1a38050311cbc61d50c93
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-01 11:46:15 +0300

    example role based view

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
@@ -1,16 +1,28 @@
 package com.spaceymonk.mentorhub.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
+import javax.annotation.security.RolesAllowed;
 
 @Controller
 public class DashboardController {
+
     @GetMapping("/dashboard")
-    public String dashboardPage(Model model, Principal principal) {
-        model.addAttribute("name", principal.getName());
-        return "features/dashboard";
+    @RolesAllowed({"ROLE_USER", "ROLE_ADMIN"})
+    public String dashboardPage(Model model, Authentication authentication) {
+        System.out.println(authentication);
+        model.addAttribute("auth", authentication);
+
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER"))) {
+            return "features/dashboard_user";
+        } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+            return "features/dashboard_admin";
+        }
+
+        return "redirect:/error";
     }
 }
