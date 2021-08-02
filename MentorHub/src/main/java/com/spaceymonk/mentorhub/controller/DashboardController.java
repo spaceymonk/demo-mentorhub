51aac64d86699240aeec65194b192fc254429dda
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-02 20:50:45 +0300

    accept reject feature

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/DashboardController.java
@@ -1,16 +1,22 @@
 package com.spaceymonk.mentorhub.controller;
 
+import com.spaceymonk.mentorhub.domain.MentorshipRequest;
 import com.spaceymonk.mentorhub.domain.User;
 import com.spaceymonk.mentorhub.repository.MentorshipRequestRepository;
 import com.spaceymonk.mentorhub.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -33,5 +39,23 @@ public class DashboardController {
         }
 
         return "redirect:/error";
+    }
+
+    @PostMapping(value = "/dashboard")
+    @RolesAllowed({"ROLE_ADMIN"})
+    @ResponseBody
+    public ResponseEntity<String> saveSubjectDetails(@RequestParam("id") String requestId,
+                                                     @RequestParam("answer") boolean answer) {
+
+        Optional<MentorshipRequest> mentorshipRequestOptional = mentorshipRequestRepository.findById(requestId);
+        if (mentorshipRequestOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        MentorshipRequest mentorshipRequest = mentorshipRequestOptional.get();
+        mentorshipRequest.setStatus((answer) ? "accept" : "reject");
+        mentorshipRequestRepository.save(mentorshipRequest);
+
+        return ResponseEntity.ok("nice");
     }
 }
