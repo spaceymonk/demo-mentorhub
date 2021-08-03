c473e230350552103059df765aa440f835c0c88b
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-03 21:45:09 +0300

    solved db latency

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/domain/MentorshipRequest.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/domain/MentorshipRequest.java
@@ -3,6 +3,7 @@ package com.spaceymonk.mentorhub.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.util.Date;
 
@@ -15,6 +16,7 @@ public class MentorshipRequest {
     private String id;
     private Subject selectedSubject;
     private String text;
+    @DBRef
     private User mentor;
     private String status;
     private Date date;
