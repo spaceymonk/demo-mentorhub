8f986b09c84bc662bbe9d1ab8acb554d319a064b
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-03 21:07:29 +0300

    unnecessary repos

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/PhaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spaceymonk.mentorhub.repository;
-
-import com.spaceymonk.mentorhub.domain.Phase;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhaseRepository extends MongoRepository<Phase, String> {
-}
--- MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/PhaseReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spaceymonk.mentorhub.repository;
-
-import com.spaceymonk.mentorhub.domain.PhaseReview;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhaseReviewRepository extends MongoRepository<PhaseReview, String> {
-}
