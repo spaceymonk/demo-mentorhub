e7e34d8978295d3c858014e2e8f9c275faee4c6e
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-04 11:42:15 +0300

    created

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/MentorshipController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/MentorshipController.java
@@ -1,19 +1,37 @@
 package com.spaceymonk.mentorhub.controller;
 
+import com.spaceymonk.mentorhub.domain.Mentorship;
+import com.spaceymonk.mentorhub.repository.MentorshipRepository;
+import com.spaceymonk.mentorhub.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class MentorshipController {
 
+    private final MentorshipRepository mentorshipRepository;
+    private final UserRepository userRepository;
+
     @GetMapping(value = "/details/{id}")
     @RolesAllowed({"ROLE_USER"})
-    public String displayMentorshipDetails(@PathVariable("id") String mentorshipId) {
-        return "";
+    public String displayMentorshipDetails(@PathVariable("id") String mentorshipId,
+                                           Model model, Authentication authentication) {
+        Optional<Mentorship> mentorshipOptional = mentorshipRepository.findById(mentorshipId);
+        if (mentorshipOptional.isEmpty()) {
+            model.addAttribute("msg", "no such mentorship!");
+            return "redirect:error";
+        }
+        model.addAttribute("mentorship", mentorshipOptional.get());
+        model.addAttribute("currentUser", userRepository.findByUsernameOrGoogleId(authentication.getName(), authentication.getName()));
+
+        return "features/mentorship-details";
     }
 }
