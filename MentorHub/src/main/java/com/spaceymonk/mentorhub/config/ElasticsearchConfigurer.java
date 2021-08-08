551935944646691ae74cbe49e3bc00fd401eda14
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-08 11:48:41 +0300

    configurer added

---
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/config/ElasticsearchConfigurer.java
@@ -0,0 +1,21 @@
+package com.spaceymonk.mentorhub.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.spaceymonk.mentorhub.repository")
+public class ElasticsearchConfigurer {
+
+    @Bean
+    RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration =
+                ClientConfiguration.builder().connectedTo("localhost:9200").build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+}
