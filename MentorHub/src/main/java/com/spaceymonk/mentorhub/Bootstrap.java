52074e19a462b05401283e9ff606e972626cded5
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-09 15:05:44 +0300

    working search query example

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/Bootstrap.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/Bootstrap.java
@@ -1,10 +1,18 @@
 package com.spaceymonk.mentorhub;
 
 import com.spaceymonk.mentorhub.domain.Role;
+import com.spaceymonk.mentorhub.domain.Subject;
 import com.spaceymonk.mentorhub.repository.*;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Component;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
 @Component
 @AllArgsConstructor
@@ -15,12 +23,20 @@ public class Bootstrap implements CommandLineRunner {
     private final MentorshipRequestRepository mentorshipRequestRepository;
     private final SubjectRepository subjectRepository;
     private final RoleRepository roleRepository;
-    private final SearchRepository searchRepository;
+    private final ElasticsearchRestTemplate elasticsearchTemplate;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Started in bootstrap...");
-        System.out.println(searchRepository.findAll());
+
+        Query searchQuery = new NativeSearchQueryBuilder()
+                .withFilter(matchQuery("majorSubject", "paradigms"))
+                .build();
+        SearchHits<Subject> articles = elasticsearchTemplate.search(searchQuery, Subject.class, IndexCoordinates.of("mentorhub.subject"));
+
+        System.out.println(articles);
+        System.out.println(articles.getSearchHit(0));
+
 
         // setup roles if they don't exist
         String[] roles = {"ROLE_USER", "ROLE_ADMIN"};
--- MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/SearchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spaceymonk.mentorhub.repository;
-
-import com.spaceymonk.mentorhub.domain.MentorshipRequest;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface SearchRepository extends ElasticsearchRepository<MentorshipRequest, String> {
-
-}
