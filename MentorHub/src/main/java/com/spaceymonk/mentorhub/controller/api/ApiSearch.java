5058521f81c4613e732b92f2b61a285a936b122a
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-09 21:18:47 +0300

    search api done

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/api/ApiSearch.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/api/ApiSearch.java
@@ -1,21 +1,64 @@
 package com.spaceymonk.mentorhub.controller.api;
 
-import com.spaceymonk.mentorhub.domain.Mentorship;
+import com.spaceymonk.mentorhub.domain.MentorshipRequest;
+import com.spaceymonk.mentorhub.repository.MentorshipRequestRepository;
 import lombok.AllArgsConstructor;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+
 
 @RequestMapping("/api/search")
 @RestController
 @AllArgsConstructor
 public class ApiSearch {
 
-    @RequestMapping(value = "/{searchTxt}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<Mentorship> findMentorship(@PathVariable("searchTxt") String searchTxt) {
-        return ResponseEntity.ok().build();
+    private final ElasticsearchRestTemplate elasticsearchRestTemplate;
+    private final MentorshipRequestRepository mentorshipRequestRepository;
+
+    static class SearchHitResponse {
+        private String id;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    @RolesAllowed({"ROLE_USER"})
+    public ResponseEntity<List<MentorshipRequest>> findMentorship(@RequestParam("searchTxt") String searchTxt) {
+
+        System.out.println("incoming search text: " + searchTxt);
+
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery("text", searchTxt)
+                        .fuzziness(Fuzziness.ONE))
+                .build();
+
+        System.out.println("query created: " + searchQuery);
+
+        SearchHits<SearchHitResponse> searchHits = elasticsearchRestTemplate
+                .search(searchQuery, SearchHitResponse.class, IndexCoordinates.of("mentorhub.mentorshiprequest"));
+
+        System.out.println("search hits acquired: " + searchHits);
+
+        List<MentorshipRequest> result = new ArrayList<>();
+        searchHits.getSearchHits().forEach(hit -> {
+            result.add(mentorshipRequestRepository.findById(Objects.requireNonNull(hit.getId())).orElse(null));
+        });
+
+        return ResponseEntity.ok(result);
     }
 
 }
