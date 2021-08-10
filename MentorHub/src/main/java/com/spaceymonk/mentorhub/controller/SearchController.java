a7ab7157b0204a90155687c077b34ac2b24a93b5
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-10 10:33:38 +0300

    nonfunctional search frontend implemented

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/SearchController.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/controller/SearchController.java
@@ -1,8 +1,9 @@
 package com.spaceymonk.mentorhub.controller;
 
-import com.spaceymonk.mentorhub.domain.MentorshipRequest;
+import com.spaceymonk.mentorhub.domain.Subject;
 import com.spaceymonk.mentorhub.domain.User;
 import com.spaceymonk.mentorhub.repository.MentorshipRequestRepository;
+import com.spaceymonk.mentorhub.repository.SubjectRepository;
 import com.spaceymonk.mentorhub.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -18,14 +19,15 @@ public class SearchController {
 
     private final MentorshipRequestRepository mentorshipRequestRepository;
     private final UserRepository userRepository;
+    private final SubjectRepository subjectRepository;
 
 
     @GetMapping("/search")
     public String searchPage(Model model, Authentication authentication) {
         User currentUser = userRepository.findByUsernameOrGoogleId(authentication.getName(), authentication.getName());
         model.addAttribute("currentUser", currentUser);
-        List<MentorshipRequest> mentorshipRequestList = mentorshipRequestRepository.findByStatus("accepted");
-        model.addAttribute("mentorshipRequestList", mentorshipRequestList);
+        List<Subject> categories = subjectRepository.findAll();
+        model.addAttribute("categories", categories);
         return "features/search";
     }
 }
