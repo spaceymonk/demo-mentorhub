6db3bb5b5cc592216e49f006270c94e59a7fafb5
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-10 11:39:36 +0300

    deleted unused methods

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/MentorshipRequestRepository.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/repository/MentorshipRequestRepository.java
@@ -1,6 +1,7 @@
 package com.spaceymonk.mentorhub.repository;
 
 import com.spaceymonk.mentorhub.domain.MentorshipRequest;
+import com.spaceymonk.mentorhub.domain.Subject;
 import com.spaceymonk.mentorhub.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -9,5 +10,4 @@ import java.util.List;
 public interface MentorshipRequestRepository extends MongoRepository<MentorshipRequest, String> {
     List<MentorshipRequest> findByStatus(String status);
 
-    List<MentorshipRequest> findByMentor(User mentor);
 }
