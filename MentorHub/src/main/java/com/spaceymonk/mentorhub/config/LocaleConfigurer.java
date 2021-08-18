cdb4f7545f7a0567180c2e31ef0e5f59f0088bc4
ozkaberktug <b.kaan.ozkan@gmail.com>
2021-08-18 13:16:01 +0300

    fix: localization

--- MentorHub/src/main/java/com/spaceymonk/mentorhub/config/LocaleConfigurer.java
+++ MentorHub/src/main/java/com/spaceymonk/mentorhub/config/LocaleConfigurer.java
@@ -1,5 +1,7 @@
 package com.spaceymonk.mentorhub.config;
 
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -23,5 +25,17 @@ public class LocaleConfigurer {
     @PostConstruct
     public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("Europe/Istanbul"));
+    }
+
+    /**
+     * Configure save timezone to Europe/Istanbul.
+     * Timezone hard-coded, it should be handled properly in future releases.
+     *
+     * @return object mapper with updated time zones
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
+        return jacksonObjectMapperBuilder ->
+                jacksonObjectMapperBuilder.timeZone(TimeZone.getTimeZone("Europe/Istanbul"));
     }
 }
